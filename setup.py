f1c728babd324c62373cc333b2c62fda0044ffae
Danny Martin <martin_daniel@bah.com>
2022-05-12 14:47:26 -0400

    use find_packages

--- setup.py
+++ setup.py
@@ -37,8 +37,7 @@ if __name__ == "__main__":
     setup(
         name=_PROJECT_NAME,
         version="0.0.0",
-        packages=[_PROJECT_NAME.replace("-", "_")],
-        package_dir = {'': 'usaspending_api'},
+        packages=find_packages(),
         install_requires=_INSTALL_REQUIRES,
         extras_require=_EXTRAS,
     )
