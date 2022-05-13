9898629df0c04710a54860839b97ed3c3895590e
Keith Hickey <hickey_keith@bah.com>
2022-05-13 14:46:10 -0400

    setting packages in setup.py, with comma

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # as some package manager environments were not able to make sense of pyproject.toml
     setup(
         name=_PROJECT_NAME,
-        packages="."
+        packages=".",
         install_requires=_INSTALL_REQUIRES,
         extras_require=_EXTRAS,
     )
